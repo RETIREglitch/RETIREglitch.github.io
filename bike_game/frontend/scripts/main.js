3e54cdaebac1034ee3db85c5d720439246502954
Devreese Jorik <jorik.devreese@student.howest.be>
2022-01-30 21:36:52 +0100

    added folliage and opponents

--- bike_game/frontend/scripts/main.js
+++ bike_game/frontend/scripts/main.js
@@ -18,6 +18,7 @@ let pointlight;
 
 // dynamic loading 
 let roadSections = [];
+
 let buildings;
 let building_pos_l = [];
 building_pos_l.push([-240,-20,50,0]); // done
@@ -37,6 +38,28 @@ building_pos_r.push([440,-76,30,3.14]); // done
 building_pos_r.push([200,-40,40,3.14]); // done
 building_pos_r.push([290,-80,30,3.14]); // done
 
+let folliage;
+let folliage_pos = [];
+folliage_pos.push([-170,45,80,0]); // done
+folliage_pos.push([-175,46,80,0]); // done
+folliage_pos.push([-170,45,80,0]); // done
+folliage_pos.push([-175,10,100,0]); // done
+folliage_pos.push([-170,30,80,0]); // done
+folliage_pos.push([-170,30,90,0]); // done
+folliage_pos.push([-170,50,80,0]); // done
+folliage_pos.push([-175,-44,60,0]); // done
+folliage_pos.push([-170,-4,80,0]); // done
+folliage_pos.push([-175,-6,80,0]); // done 
+folliage_pos.push([-170,-4,80,0]); // done
+folliage_pos.push([-175,40,130,0]); // done
+folliage_pos.push([-170,-70,90,0]); // done
+folliage_pos.push([-175,-68,80,0]); // done
+folliage_pos.push([-170,-70,80,0]); // done
+folliage_pos.push([-175,-60,80,0]); // done
+folliage_pos.push([-160,-66,50,0]); // done
+folliage_pos.push([-120,-62,40,0]); // done
+
+
 // finish line: z-pos should be -53965;
 let finish_line;
 
@@ -52,8 +75,12 @@ let random_x_length = Math.random() * 5;
 let bike_y_change = 0;
 let moved_y = 0;
 
+let otherBikes = [0*2];
+var randomBikeX;
+
 // other data
 let paused = true;
+let finished = false;
 
 // html data
 let htmlSpeed;
@@ -124,18 +151,18 @@ class Buildings{
         this.buildings_r = [];
         this.initialize();
     }
+
     initialize(){
         this.loadBuildings();
     }
 
     loadBuildings(){
+        const gltfLoader = new THREE.GLTFLoader();
         for (let i = 0; i < 7; i++) {
             let model = `models/buildings/building${i+1}.glb`;
-            const gltfLoader = new THREE.GLTFLoader();
             // left side
             gltfLoader.load(model, (gltf) => {
                 const building = gltf.scene;
-                building.visible = false;
                 let scalef = building_pos_l[i][2];
                 building.scale.set(scalef, scalef, scalef);
                 building.position.z = 1000;
@@ -145,22 +172,21 @@ class Buildings{
 
             gltfLoader.load(model, (gltf) => {
                 const building = gltf.scene;
-                building.visible = false;
                 let scalef = building_pos_r[i][2];
                 building.scale.set(scalef, scalef, scalef);
                 building.position.z = 1000;
                 scene.add(building);
                 this.buildings_r.push(building);
                 if (i == 6) {
-                    this.updateAllBuildings()
-                    loadBike("models/bike.obj");
+                    this.updateAllBuildings();
+                    folliage = new Folliage();
                 }
             })
         }
     }
     
     updateAllBuildings() {
-        for (let i = 0; i < 7; i++) {
+        for (let i = 0; i < this.buildings_l.length; i++) {
             this.buildings_l[i].position.x += building_pos_l[i][0];
             this.buildings_l[i].position.y += building_pos_l[i][1];
 
@@ -180,7 +206,6 @@ class Buildings{
         let randomValue = randomInt(0,buildingsArray.length-1);
         let building = buildingsArray[randomValue];
         while (building.position.z < bike.position.z) {
-            console.log("test");
             buildingsArray = this.buildings_l;
             if (randomInt(0,1) == 1) {
                 buildingsArray = this.buildings_r;
@@ -190,11 +215,72 @@ class Buildings{
         }
         
         building.position.z = z;
-        building.visible = true;
         //console.log(randomValue);
         return;
 
     }
+}
+
+class Folliage {
+    constructor(){
+        this.folliage = [0*18];
+        this.initialize()
+    }
+
+    initialize(){
+        this.loadFolliage();
+    }
+
+    loadFolliage(){
+        let loadedcount = 0;
+        const gltfLoader = new THREE.GLTFLoader();
+        for (let i = 0; i < 18; i++) {
+            let model = `models/folliage/folliage${i+1}.glb`;
+            // left side
+            gltfLoader.load(model, (gltf) => {
+                loadedcount++;
+                const folliageModel = gltf.scene;
+                let scalef = folliage_pos[i][2];
+                folliageModel.scale.set(scalef, scalef, scalef);
+                folliageModel.position.z = 1000;
+                scene.add(folliageModel);
+                this.folliage[i] = folliageModel;
+                if (loadedcount == 18) {
+                    this.updateAllFolliage();
+                    loadBike("models/bike.obj");
+                }
+            })
+        }
+    }
+
+    updateAllFolliage(){
+        for (let i = 0; i < this.folliage.length; i++) {
+            this.folliage[i].position.x += folliage_pos[i][0];
+            this.folliage[i].position.y += folliage_pos[i][1];
+
+        }
+
+    }
+
+    addRandomFolliage(z){
+        let randomValue = randomInt(0,this.folliage.length-1); //fm
+        let folliageModel = this.folliage[randomValue];
+
+        while (folliageModel.position.z < bike.position.z) {
+            randomValue = randomInt(0,this.folliage.length-1);
+            folliageModel = this.folliage[randomValue];
+        }
+
+        if (randomInt(0,1) == 1) { // 50% chance of moving the folliageModel ot the other side of the road
+            folliageModel.position.x = -folliageModel.position.x;
+        }
+
+        folliageModel.position.z = z;
+        //console.log(randomValue);
+        return;
+
+    }
+
 }
 
 class FinishLine{
@@ -204,7 +290,6 @@ class FinishLine{
     }
 
     initialize(){
-        console.log("creating end")
         var geometry = new THREE.BoxGeometry(4, 160, 4, 1, 1, 1);
         var texture = new THREE.TextureLoader().load("img/wood_texture.png");
         var material = new THREE.MeshStandardMaterial({map:texture});
@@ -237,13 +322,73 @@ function loadBike(model) {
     const objLoader = new THREE.OBJLoader();
     objLoader.load(model, function(obj){
         obj.position.y = -140;
-        obj.position.z = -18;
         obj.position.z = -20;
         scene.add(obj);
         bike = obj;
-        buildings.addRandomBuilding(-200);
-        buildings.addRandomBuilding(-400);
+        loadBikes(model)
     });
+
+}
+
+class Opponent{
+    constructor(avg_speed,bike){
+        this.initial_speed = avg_speed;
+        this.avg_speed = avg_speed;
+        this.bike = bike;
+        this.initialize();
+        this.timeuntilsprint = randomInt(500,700);
+        this.sprint = false;
+    }
+
+    initialize(){
+        this.bike.scale.set(0.4,0.4,0.4);
+    }
+
+    move(){
+        this.bike.position.z += -this.avg_speed/2;
+        this.timeuntilsprint --;
+        if (this.timeuntilsprint == 0) {
+            console.log("sprinting!")
+            this.sprint != this.sprint;
+            if (this.sprint) {
+                this.avg_speed += random(2,5);
+                this.timeuntilsprint = randomInt(100,200);
+                return;
+            }
+            this.avg_speed = this.initial_speed;
+            this.timeuntilsprint = randomInt(500,700);
+        }
+    }
+
+}
+
+function loadBikes(model){
+    const objLoader = new THREE.OBJLoader();
+    objLoader.load(model, function(obj){
+        obj.position.y = -80;
+        obj.position.x = -80;
+        obj.position.z = -20;
+        scene.add(obj);
+        otherBikes[0] = new Opponent(21.8,obj);
+    });
+    objLoader.load(model, function(obj){
+        obj.position.y = -80;
+        obj.position.x = 80;
+        obj.position.z = -20;
+        scene.add(obj);
+        otherBikes[1] = new Opponent(23.6,obj);
+    });
+
+
+    buildings.addRandomBuilding(-200);
+    buildings.addRandomBuilding(-400);
+
+    folliage.addRandomFolliage(-100);
+    folliage.addRandomFolliage(-300);
+    folliage.addRandomFolliage(-200);
+    folliage.addRandomFolliage(-400);
+    folliage.addRandomFolliage(-500);
+    folliage.addRandomFolliage(-600);
 
 }
 
@@ -293,25 +438,14 @@ function updatePlayer() {
     //player.z += -player.speed/2;
     camera.translateZ(-player.speed/2);
     bike.position.z += -player.speed/2;
-    console.log(bike.position.z);
-
-    if (roadSections[0].z > bike.position.z+260) {
-        roadSections[0].updateZ(-400*roadSections.length+1);
-        let tempPart = roadSections[0];
-
-        for (let i = 0; i < roadSections.length-1;i++){
-            roadSections[i] = roadSections[i+1];
-        }
-        roadSections[roadSections.length-1] = tempPart;
-        buildings.addRandomBuilding(bike.position.z-1000);
-    }
+    // console.log(bike.position.z);
 
     // if (pointlight.position.z > bike.position.z+100) {
     //     pointlight.position.z -= 200;
     // }
     pointlight.position.z = bike.position.z;
 
-    const randomBikeX = pseudoRandomX();
+    randomBikeX = pseudoRandomX();
     bike.position.x += randomBikeX;
     bike.rotation.y += randomBikeX/200;
     bike.rotation.z += randomBikeX/200;
@@ -321,7 +455,10 @@ function updatePlayer() {
     let displayDistance = player.remainingDistance - player.remainingDistance%1
 
     if (displayDistance <= 0) {
-        alert('You reached the goal!'); 
+        if (!finished){
+            alert('You reached the goal!'); 
+            finished = true;
+        }
         displayDistance = 0;
     }
     if (htmlDistance) {
@@ -330,12 +467,40 @@ function updatePlayer() {
     
 }
 
+function updateRoads(){
+    if (roadSections[0].z > bike.position.z+260) {
+        roadSections[0].updateZ(-400*roadSections.length+1);
+        let tempPart = roadSections[0];
+
+        for (let i = 0; i < roadSections.length-1;i++){
+            roadSections[i] = roadSections[i+1];
+        }
+        roadSections[roadSections.length-1] = tempPart;
+        buildings.addRandomBuilding(bike.position.z-1000);
+        folliage.addRandomFolliage(bike.position.z-1300);
+        folliage.addRandomFolliage(bike.position.z-1200);
+        folliage.addRandomFolliage(bike.position.z-1100);
+
+    }
+}
+
+function updateOpponents() {
+    for (let opponent of otherBikes){
+        opponent.move();
+        opponent.bike.position.x += randomBikeX*2;
+        opponent.bike.rotation.y += randomBikeX/100;
+        opponent.bike.rotation.z += randomBikeX/100;
+    }
+}
+
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene,camera);
     
     if (!paused) {
         updatePlayer();
+        updateRoads();
+        updateOpponents();
         // window.requestAnimationFrame(render);
     }
 }
@@ -351,7 +516,7 @@ function onKeyPressed() {
 
         if (code == "Space") {
             paused = !paused;
-            console.log(`paused game ${paused}`);
+            //console.log(`paused game ${paused}`);
             if (paused) {
                 track.pause();
                 return;
@@ -391,13 +556,12 @@ document.addEventListener("DOMContentLoaded", function () {
     track.play();
 
         
-    const background = new THREE.TextureLoader().load("img/skybox.png");
+    const background = new THREE.TextureLoader().load("img/background.png");
     scene.background = background;
 
     pointlight = new THREE.PointLight(0xFFFFCC);
     pointlight.position.set(0,100,100);
     scene.add(pointlight);
-    console.log(pointlight);
     pointlight.decay = 0;
 
     onKeyPressed();
